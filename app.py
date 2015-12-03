0b48a1768a050beed4363ca98d3316a456627857
kjabbe <kjabbe@gmail.com>
2015-12-04 00:44:20 +0100

    more testing

--- app.py
+++ app.py
@@ -1,4 +1,3 @@
-from snmp_client import SNMPClient
 from flask import Flask, redirect, render_template, url_for
 
 # Initiate Flask
@@ -6,25 +5,12 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return redirect(url_for('cim_dashboard'))
+    return redirect(url_for('test'))
 
 # SNMP dashboard method
-@app.route("/snmp")
+@app.route("/test")
 def snmp_dashboard():
-    snmp = SNMPClient()
-
-    try:
-        os_info = snmp.getOs()
-    except:
-        os_info = "Host not availible"
-    
-    try:
-        ip_interfaces = snmp.getNetwork()
-    except:
-        ip_interfaces = [] 
-    
-    return render_template('snmp.html',
-        os_info=os_info, ip_interfaces=ip_interfaces)#, url_for=url_for())
+    return render_template('test.html') # os_info=os_info, ip_interfaces=ip_interfaces)#, url_for=url_for())
 
 # Run the server
 if __name__ == "__main__":
