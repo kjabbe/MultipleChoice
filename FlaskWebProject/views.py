d8df013f2d6fa790ca8eef0144b87f363a4641ba
kjabbe <kjabbe@gmail.com>
2015-12-05 16:05:12 +0100

    changed name of answer check

--- FlaskWebProject/views.py
+++ FlaskWebProject/views.py
@@ -55,7 +55,7 @@ def questions(theme):
 	)
 
 @app.route('/questions/<title>/<item>/<qid>/<cur>')
-def testFun(title, item, qid, cur):
+def checkAnswer(title, item, qid, cur):
 	"""Renders the about page."""
 	cur = ast.literal_eval(cur)
 	mod = qid
