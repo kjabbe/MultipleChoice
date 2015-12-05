f2ee6fc31583721da6318c8df1d192fc44930b40
kjabbe <kjabbe@gmail.com>
2015-12-05 16:12:55 +0100

    disabled debug

--- FlaskWebProject/views.py
+++ FlaskWebProject/views.py
@@ -10,7 +10,7 @@ import os
 APP_ROOT = os.path.dirname(os.path.abspath(__file__))   # refers to application_top
 APP_STATIC = os.path.join(APP_ROOT, 'static')
 
-app.debug = True
+#app.debug = True
 
 @app.route('/')
 @app.route('/home')
